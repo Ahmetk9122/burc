6e5d642c8ce869edf46dac94c6ae53266026fbc7
Ahmet Karabudak <ahmet.karabudakk.9122@gmail.com>
2021-11-24 13:52:33 +0300

    Modified

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.purple,
       ),
      onGenerateRoute: RouteGenerator.routeGenerator,
     );
